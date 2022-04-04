3745264591f7844d2e559fb86e99ce8fd8b5cb2d
Grigoriy Martirosov <grigori96@gmail.com>
2022-04-04 19:05:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# russian_bank_icons_flutter
+
+Icons library, based on [Top Russian Banks Icons](https://github.com/sergey-tkach/top-russian-banks-icons)
+
+## Credits
+
+- https://github.com/sergey-tkach/top-russian-banks-icons
---
+++ lib/russian_banks_icons_flutter.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/widgets.dart';
+
+class TopRussianBanksIcons {
+  TopRussianBanksIcons._();
+
+  static const _kFontFam = 'TopRussianBanksIcons';
+  static const String? _kFontPkg = "top_russian_banks_icons";
+
+  static const IconData absolut = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData akbars = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData alfa = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData atb = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData avangard = IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData aversbank = IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData bank_soyuz = IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData bbr = IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData binbank = IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData bks = IconData(0xe809, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData bnp_paribas = IconData(0xe80a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData bystrobank = IconData(0xe80b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData chelindbank = IconData(0xe80c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData cetelem = IconData(0xe80d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData citi = IconData(0xe80e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData crediteurope = IconData(0xe80f, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData domrfbank = IconData(0xe810, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData dvb = IconData(0xe811, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData expobank = IconData(0xe812, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData forabank = IconData(0xe813, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData gazprom = IconData(0xe814, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData homecredit = IconData(0xe815, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData ing = IconData(0xe816, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData intesa = IconData(0xe817, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData lipetskkombank = IconData(0xe818, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData lockobank = IconData(0xe819, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData mdm = IconData(0xe81a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData metallinvestbank = IconData(0xe81b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData metcom = IconData(0xe81c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData minbank = IconData(0xe81d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData mkb = IconData(0xe81e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData modulbank = IconData(0xe81f, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData moskva = IconData(0xe820, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData mosoblbank = IconData(0xe821, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData ndb = IconData(0xe822, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData mts = IconData(0xe823, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData neyva = IconData(0xe824, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData novikom = IconData(0xe825, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData open = IconData(0xe826, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData otp = IconData(0xe827, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData peresvet = IconData(0xe828, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData plus_bank = IconData(0xe829, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData pochta = IconData(0xe82a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData poidem = IconData(0xe82b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData primbank = IconData(0xe82c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData raiffeisen = IconData(0xe82d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData rencredit = IconData(0xe82e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData rgsbank = IconData(0xe82f, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData psb = IconData(0xe830, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData qiwi = IconData(0xe831, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData rnkb = IconData(0xe832, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData rocket = IconData(0xe833, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData ros = IconData(0xe834, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData rosevro = IconData(0xe835, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData rosselkhoz = IconData(0xe836, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData rossiya = IconData(0xe837, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData rs = IconData(0xe838, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData sber = IconData(0xe839, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData skbbank = IconData(0xe83a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData smpbank = IconData(0xe83b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData snbank = IconData(0xe83c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData sovcombank = IconData(0xe83d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData spb = IconData(0xe83e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData surgutneftegas = IconData(0xe83f, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData sviaz_bank = IconData(0xe840, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData tatsotsbank = IconData(0xe841, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData tavricheskiy = IconData(0xe842, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData tinkoff = IconData(0xe843, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData tkb = IconData(0xe844, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData tochka = IconData(0xe845, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData ubrir = IconData(0xe846, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData unicredit = IconData(0xe847, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData trust = IconData(0xe848, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData uralsib = IconData(0xe849, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData vbrr = IconData(0xe84a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData vozrozhdenie = IconData(0xe84b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData vostbank = IconData(0xe84c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData vtb = IconData(0xe84d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData zapsibkombank = IconData(0xe84e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData zenit = IconData(0xe84f, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+
+  static const Map<String, IconData> iconsByName = {
+    "absolut": absolut,
+    "akbars": akbars,
+    "alfa": alfa,
+    "atb": atb,
+    "avangard": avangard,
+    "aversbank": aversbank,
+    "bank_soyuz": bank_soyuz,
+    "bbr": bbr,
+    "binbank": binbank,
+    "bks": bks,
+    "bnp_paribas": bnp_paribas,
+    "bystrobank": bystrobank,
+    "cetelem": cetelem,
+    "chelindbank": chelindbank,
+    "citi": citi,
+    "crediteurope": crediteurope,
+    "domrfbank": domrfbank,
+    "dvb": dvb,
+    "expobank": expobank,
+    "forabank": forabank,
+    "homecredit": homecredit,
+    "gazprom": gazprom,
+    "lipetskkombank": lipetskkombank,
+    "intesa": intesa,
+    "lockobank": lockobank,
+    "ing": ing,
+    "mdm": mdm,
+    "metallinvestbank": metallinvestbank,
+    "metcom": metcom,
+    "minbank": minbank,
+    "mkb": mkb,
+    "modulbank": modulbank,
+    "moskva": moskva,
+    "mosoblbank": mosoblbank,
+    "mts": mts,
+    "ndb": ndb,
+    "neyva": neyva,
+    "novikom": novikom,
+    "open": open,
+    "otp": otp,
+    "peresvet": peresvet,
+    "plus_bank": plus_bank,
+    "pochta": pochta,
+    "poidem": poidem,
+    "primbank": primbank,
+    "psb": psb,
+    "qiwi": qiwi,
+    "raiffeisen": raiffeisen,
+    "rgsbank": rgsbank,
+    "rencredit": rencredit,
+    "rnkb": rnkb,
+    "rocket": rocket,
+    "ros": ros,
+    "rosevro": rosevro,
+    "rosselkhoz": rosselkhoz,
+    "rossiya": rossiya,
+    "rs": rs,
+    "sber": sber,
+    "skbbank": skbbank,
+    "snbank": snbank,
+    "smpbank": smpbank,
+    "sovcombank": sovcombank,
+    "spb": spb,
+    "surgutneftegas": surgutneftegas,
+    "sviaz_bank": sviaz_bank,
+    "tatsotsbank": tatsotsbank,
+    "tavricheskiy": tavricheskiy,
+    "tinkoff": tinkoff,
+    "tkb": tkb,
+    "tochka": tochka,
+    "trust": trust,
+    "ubrir": ubrir,
+    "unicredit": unicredit,
+    "uralsib": uralsib,
+    "vbrr": vbrr,
+    "vostbank": vostbank,
+    "vozrozhdenie": vozrozhdenie,
+    "vtb": vtb,
+    "zapsibkombank": zapsibkombank,
+    "zenit": zenit
+  };
+}
